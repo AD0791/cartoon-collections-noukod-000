637ed2b6e0ba3fcd7473ad097e700d336f1d3c39
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-24 01:55:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls appel
   appel.any? { |e| e.length > 4  }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese cheesal
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
