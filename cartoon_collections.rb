cb9a03caed9a79017ae99ee9cedd90c9974e9c35
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-24 01:51:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet collections
   collections.map { |e| "#{e.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls appel
+  appel.any? { |e| e.length > 4  }
 end
 
 def find_the_cheese# code an argument here
